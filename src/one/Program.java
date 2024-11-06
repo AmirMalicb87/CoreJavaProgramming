15345018b94b6506dece13590dd2d38aeded078f
AmirMalicb87 <amirmalicb@gmail.com>
2024-11-06 18:41:24 +0100

    Ovo je moja inicijalna verzija

---
+++ src/one/Program.java
@@ -0,0 +1,9 @@
+package one;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Vozdra raja sta ima haaa");
+        System.out.println("Kako ste mi danas ?");
+    }
+}
